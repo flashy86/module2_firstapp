1a0185d496785eee9e44f9cc183f1e1a97a3384b
flashy86 <annecharlotte.lucas2021@campus-eni.fr>
2021-11-08 19:22:36 +0100

    Mise en place du statefull widget + debut de style

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main(List<String> args) {
   runApp(MyApp());
 }
@@ -8,15 +10,58 @@ void main(List<String> args) {
 
 //Pour reformater le code: Maj + Alt + F
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+
+    print(_questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First Cool App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            // Text(questions.first),
+            Question(
+              questions[_questionIndex],
+            ),
+            RaisedButton(
+              child: Text('Answer1'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: () => print('Answer chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer3'),
+              onPressed: () => {
+                // instructions supplémentaires
+                print('Answer chosen !')
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
